73afdfc85752cf480e6862a25ea0993868ff9a00
dennisochieng <ochiengdennis32@gmail.com>
2025-06-14 19:39:24 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -6,9 +6,37 @@ app = Flask(__name__)
 def index():
     return '<h1>Welcome to my page!</h1>'
 
-@app.route('/<string:username>')
-def user(username):
-    return f'<h1>Profile for {username}</h1>'
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)
+    return f'<p> {parameter}</p>'
+
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    numbers = '<br>'.join(str(i) for i in range(parameter + 1))
+    return numbers
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    result = None
+
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 == 0:
+            return '<p>Error: Division by zero.</p>'
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return '<p>Invalid operation. Use +, -, *, div, or %.</p>'
+
+    return f'<p>{num1} {operation} {num2} = {result}</p>'
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
